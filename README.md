52fbbdb290e607316d94617cfa23edec344744bf
Joyce Kim <joycekim@Joyces-MacBook-Pro.local>
2018-01-28 17:25:20 -0500

    integrated cloudinary upload; need persistence

---
+++ README.md
@@ -0,0 +1,25 @@
+# Fics
+
+## Pages
+
+1. Explore: 
+	- users can search for food item via search bar
+	- each photo contains location and uploader's comment
+	- users can go to upload their own photo
+
+2. Upload:
+	- users can upload their photo with their own comment
+
+## To Do
+
+1. Set up database/routes/api/controllers(what is this)
+2. Figure out how photo data sits in database so we can then figure out how to display this data/photo on Explore page
+3. make dummy photo data in db
+4. make Upload component such that users can post their photos and comments to the db
+5. render photos to explore page
+6. make search bar to filter through photos (all photos will show unless "food term" searched; after a search, photos will only show those related to the "food term")
+7. clicking a photo will either direct to page about the photo or show another div instead of the photo with location and uploader's comment
+8. beautify
+
+## Backlog
+1. Ability to comment on photos
--- client/src/pages/Upload/Upload.js
+++ client/src/pages/Upload/Upload.js
@@ -1,9 +1,84 @@
 import React, { Component } from "react";
+import Dropzone from 'react-dropzone';
+import sha1 from 'sha1';
+import superagent from 'superagent';
 
 class UploadPage extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      images: []
+    };
+  }
+  
+  uploadFile(files){
+    console.log('uploadFile: ');
+    const image = files[0];
+
+    const cloudName = 'ddn00kubl';
+    const url = 'https://api.cloudinary.com/v1_1/' + cloudName + '/image/upload';
+
+    const timestamp = Date.now()/1000
+    const uploadPreset = 'qbs7mxp8';
+
+    const paramsStr = 'timestamp=' + timestamp + '&upload_preset=' + uploadPreset + 'dgK4gcLQX_6AupWSB7csv5R4U68';
+
+    const signature = sha1(paramsStr);
+
+    const params = {
+      'api_key': '714671874936964',
+      'timestamp': timestamp,
+      'upload_preset': uploadPreset,
+      'signature': signature
+    };
+
+    let uploadRequest = superagent.post(url);
+    uploadRequest.attach('file', image);
+
+    Object.keys(params).forEach((key) => {
+      uploadRequest.field(key, params[key])
+    });
+
+    uploadRequest.end((err, resp) => {
+      if (err){
+        alert(err);
+        return;
+      }
+
+      console.log("UPLOAD COMPLETE: " + JSON.stringify(resp.body));
+      const uploaded = resp.body;
+
+      let updatedImages = Object.assign([], this.state.images);
+      updatedImages.push(uploaded);
+
+      this.setState({
+        images: updatedImages
+      })
+    })
+
+  }
+
   render() {
+
+    const list = this.state.images.map((image, i) => {
+      return (
+        <li key={i}>
+          <img style={{width:72}} src={image.secure_url} /> 
+        </li>
+      )
+    })
+
     return (
-      <h1>This is where you upload food photos and their review</h1>
+      <div>
+        <h1>This is where you upload food photos and their review</h1>
+        <Dropzone 
+          onDrop={this.uploadFile.bind(this)}
+        />
+        <ol>
+          {list}
+        </ol>
+      </div>
     );
   }
 }
