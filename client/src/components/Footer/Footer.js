fe2bce39f57287777a3cb29d973aa3769e750482
John Hillegass <hillegassdev@gmail.com>
2018-01-29 00:10:54 -0500

    update footer with note about where images are coming from, pixabay credit

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,7 +3,8 @@ import "./Footer.css";
 
 const Footer = () =>
   <footer className="footer">
-    <p>Made with <i className="fa fa-heart" aria-hidden="true" style={{"color": "red"}}></i> by Parwiz</p>
+    <p>Made with <i className="fa fa-heart" aria-hidden="true" style={{"color": "red"}}></i> by Team C</p>
+    <p> Images from Pixabay </p>
   </footer>;
 
 export default Footer;
