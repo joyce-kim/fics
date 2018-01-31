9430d1f7ffde6e6b0ac9bcdf17085c497ca4abf6
John Hillegass <hillegassdev@gmail.com>
2018-01-30 23:01:39 -0500

    Add Image Component

---
+++ client/src/components/Image/Image.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Image.css';
+
+class Image extends Component{
+  constructor(props){
+    super(props);
+    this.state ={}
+  }
+  render(){
+    return(
+
+    )
+  }
+}
+
+export default Searchbox
